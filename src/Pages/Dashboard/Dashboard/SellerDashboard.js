e9f3a7302ade4095cc9fe1f9f17305c253c1a03a
alamin09876 <alamin.ahmed0795@gmail.com>
2022-11-28 21:51:10 +0600

    user

--- src/Pages/Dashboard/Dashboard/SellerDashboard.js
+++ src/Pages/Dashboard/Dashboard/SellerDashboard.js
@@ -6,12 +6,11 @@ import Category from '../Category/Category';
 const SellerDashboard = () => {
     
     return (
-        <div className=' grid lg:grid-cols-3 '>
+        <div className=' grid lg:grid-cols-2 gap-10 px-24'>
             <Link className='mb-6 card-body text-white text-center bg-slate-600 ' to='/addproducts'>Add products</Link>
-            <Link className='mb-6 card-body text-white text-center bg-slate-600 ' to='/myproducts'>My products</Link>
-        
-            <Category></Category>
+            <Link className='mb-6 card-body text-white text-center bg-slate-600 ' to='/myproducts'>My products</Link>        
             <label className='my-6 card-body text-white text-center  bg-slate-600 ' htmlFor="my-modal-6">Category</label>
+            <Category></Category>
             
 
         </div>
