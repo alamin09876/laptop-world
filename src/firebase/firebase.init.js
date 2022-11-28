ae1c97ebd83390b9f55648713a3c856f7365f0f5
alamin09876 <alamin.ahmed0795@gmail.com>
2022-11-28 23:43:39 +0600

    env

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -25,12 +25,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBjkAR8-KuXaTo5TpM1RkYdP9Ns0V1tX6E",
-  authDomain: "laptopworld-4aada.firebaseapp.com",
-  projectId: "laptopworld-4aada",
-  storageBucket: "laptopworld-4aada.appspot.com",
-  messagingSenderId: "293535901153",
-  appId: "1:293535901153:web:79d75c3df8681b81bddea0"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
